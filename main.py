a1e038d97e7d288c799f12f4aefafd632def594f
jspeed-meyers <jmeyers@iqt.org>
2021-09-29 19:43:47 -0400

    Add warnings for PyPthon missing data

--- main.py
+++ main.py
@@ -2,12 +2,7 @@
 
 import argparse
 
-from pypi import (
-    get_github_url_from_pypi_json,
-    get_pypi_data_json,
-    get_pypi_package_dependencies,
-    parse_requirements_dot_text,
-)
+from pypi import python_requirements_dot_text_analysis
 
 
 def parse_command_line_arguments():
@@ -30,25 +25,4 @@ if __name__ == "__main__":
     # parse specified Python requirements.txt file and generate GitHub links
     # for all top-level AND transitive dependencies.
     if args.python:
-
-        top_level_pkgs = parse_requirements_dot_text(args.python)
-
-        # Retrieve all dependencies, both top-level and transitive, and keep a
-        # unique list
-        all_pkgs = []
-        for pkg in top_level_pkgs:
-            all_deps = get_pypi_package_dependencies(pkg)
-            for dep in all_deps:
-                if dep not in all_pkgs:
-                    all_pkgs.append(dep)
-
-        # retrieve github urls for unique pypi packages
-        github_urls = []
-        for pkg in all_pkgs:
-            pypi_json = get_pypi_data_json(pkg)
-            github_url = get_github_url_from_pypi_json(pypi_json)
-            if github_url:
-                github_urls.append(github_url)
-
-        for url in github_urls:
-            print(url)
+        python_requirements_dot_text_analysis(args.python)
