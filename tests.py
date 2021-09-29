4e5cd7d1178157da1ca9c351d1a1d42dcc9237bd
jspeed-meyers <jmeyers@iqt.org>
2021-09-29 14:23:07 -0400

    Add tests for parsing python requirements.txt

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from pypi import (
     get_github_url_from_pypi_json,
     get_pypi_data_json,
     get_pypi_package_dependencies,
+    parse_requirements_dot_text,
 )
 
 # pylint: disable="attribute-defined-outside-init"
@@ -39,6 +40,16 @@ class TestPypiMethods(unittest.TestCase):
         self.requests_test_deps = get_pypi_package_dependencies("requests")
         self.assertEqual(len(self.requests_test_deps), 5)
         self.assertTrue(self.requests_test_deps["certifi"])
+
+    def test_parse_requirements_dot_text(self):
+        """Check parsing requirements.txt files"""
+        self.test_requirements = parse_requirements_dot_text(
+            "test/test_requirements.txt"
+        )
+        self.assertEqual(len(self.test_requirements), 3)
+        self.assertEqual(
+            self.test_requirements, ["package_a", "package_b", "package_c"]
+        )
 
 
 if __name__ == "__main__":
