bedbc696ab55e7c69381da4291a563259ffa997c
William Spear <spearw@gmail.com>
2021-11-10 09:54:07 -0600

    Update TestBiocondaMethods with new recipe parsing

--- tests.py
+++ tests.py
@@ -183,18 +183,25 @@ class TestBiocondaMethods(unittest.TestCase):
     def test_parse_bioconda_meta_dot_yaml(self):
         """Check parsing package.toml files"""
         self.test_bioconda_yaml = parse_meta_dot_yaml_for_source_link("test/meta.yaml")
-        self.assertEqual(self.test_bioconda_yaml, "https://github.com/prihoda/AbNumber")
+        self.assertEqual(
+            self.test_bioconda_yaml, ["https://github.com/prihoda/abnumber"]
+        )
 
     def test_generate_bioconda_source_links(self):
         """Check generate_bioconda_source_links() function."""
-        self.test_bioconda_source_links = generate_bioconda_source_links(".")
-        self.assertTrue(
-            "https://github.com/prihoda/AbNumber" in self.test_bioconda_source_links
+        self.test_bioconda_source_links = generate_bioconda_source_links(
+            "test/bioconda_package_tree"
         )
         self.assertTrue(
-            "https://github.com/marekborowiec/AMAS" in self.test_bioconda_source_links
+            "https://github.com/prihoda/abnumber" in self.test_bioconda_source_links
         )
-        self.assertEqual(len(self.test_bioconda_source_links), 2)
+        self.assertTrue(
+            "https://github.com/ablab/spades" in self.test_bioconda_source_links
+        )
+        self.assertTrue(
+            "https://github.com/tseemann/shovill" in self.test_bioconda_source_links
+        )
+        self.assertEqual(len(self.test_bioconda_source_links), 3)
 
 
 class TestUtilsMethods(unittest.TestCase):
